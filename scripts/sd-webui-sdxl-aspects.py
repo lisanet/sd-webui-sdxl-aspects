31fe65a7b91d6061a25a564fa72c915e5eddda20
Simone Karin Lehmann <simone@lisanet.de>
2023-08-25 15:54:50 +0200

    add crop and overwrite original. See README for more information

--- scripts/sd-webui-sdxl-aspects.py
+++ scripts/sd-webui-sdxl-aspects.py
@@ -1,6 +1,8 @@
 import gradio as gr
 from modules.ui_components import FormRow
 import modules.scripts as scripts
+from modules import images
+from modules.shared import opts
 
 
 class AspectScript(scripts.Script):
@@ -45,10 +47,67 @@ class AspectScript(scripts.Script):
                     slider = gr.Slider(0, 6, step=1, label="Aspect type", elem_id=f'{tab}_sdxl_aspect_type')
                 with gr.Column(min_width=50, scale=1):
                     ar = gr.Textbox("1:1", label="Aspect ratio", show_label=True, interactive=False, elem_id=f'{tab}_sdxl_aspect_ar')
+            with FormRow():
+                crop = gr.Checkbox(label="Crop to fit aspect ratio", value=False, elem_id=f'{tab}_sdxl_aspect_crop')
+                overwrite = gr.Checkbox(label="Overwrite uncropped image", value=False, elem_id=f'{tab}_sdxl_aspect_overwrite')
             gr.Markdown()
         slider.change(self.get_aspect, inputs=[slider], outputs=[ar, self.dims_w, self.dims_h], show_progress=False)
+        
+        return [slider, ar, crop, overwrite]
 
-        return [slider, ar]
+
+    def crop_image(self, img):
+        w = img.width
+        h = img.height
+        ar_w = self.ar_w
+        ar_h = self.ar_h
+        # if orientation is different, switch aspect ratio
+        if (w - h) * (self.ar_w - self.ar_h) < 0:
+            ar_w = self.ar_h
+            ar_h = self.ar_w
+            
+        # calc new size to match aspect, round to nearest even num. 
+        # This will give exakt pixel aspect ratios, 
+        # except for 3:2 and 16:9 where the difference is 0.66 pixels in only one dimension :-)
+        if w / ar_w * ar_h > h:
+            crop_w = int((h / ar_h * ar_w + 1)/2) * 2
+            crop_h = h
+        else:
+            crop_w = w
+            crop_h = int((w / ar_w * ar_h + 1)/2) * 2
+
+        top = (h - crop_h) / 2
+        left = (w - crop_w) / 2
+        bottom = top + crop_h
+        right = left + crop_w 
+        return img.crop((left, top, right, bottom ))
+
+
+    def process(self, p, slider, ar, crop, overwrite):
+        if crop:
+            p.extra_generation_params["Ascpet ratio"] = ar
+            p.extra_generation_params["Ascpet ratio crop"] = crop
+            if overwrite:
+                p.do_not_save_samples = True
+
+
+    def postprocess(self, p, processed, slider, ar, crop, overwrite):
+        if crop:
+            count = len(processed.images)
+            index = 0
+            self.get_aspect(slider)
+            unwanted_grid = count < 2 and opts.grid_only_if_multiple
+            
+            for i in range(count):
+                if i == 0 and not p.do_not_save_grid and not unwanted_grid: # skip grid
+                    continue
+
+                processed.images[i] = self.crop_image(processed.images[i])
+                suffix = "" if overwrite else "-crop"
+                img_info = processed.infotext(p, index)
+                images.save_image(processed.images[i], p.outpath_samples, "",
+                   processed.seed + index, processed.prompt, opts.samples_format, info = img_info, p = p, suffix = suffix)
+                index += 1
 
 
     # https://github.com/AUTOMATIC1111/stable-diffusion-webui/pull/7456#issuecomment-1414465888
